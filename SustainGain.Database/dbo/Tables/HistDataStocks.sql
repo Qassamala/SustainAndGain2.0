fd40da58f2c8d288ede907154e029070a2bf7dad
Daniel <daniel.lofqvist93@gmail.com>
2020-01-21 11:56:40 +0100

    Altered HistDataStocks

--- SustainGain.Database/dbo/Tables/HistDataStocks.sql
+++ SustainGain.Database/dbo/Tables/HistDataStocks.sql
@@ -1,8 +1,9 @@
-﻿CREATE TABLE [dbo].[Table]
+﻿Create TABLE [dbo].[HistDataStocks]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [StocksID] INT NOT NULL FOREIGN KEY REFERENCES [StaticStockData](Id), 
+    [StocksID] INT NOT NULL, 
     [DateTime] DATETIME NOT NULL, 
-    [CurrentPrice] MONEY NULL
+    [CurrentPrice] MONEY NULL,
+    FOREIGN KEY (StocksID) REFERENCES [StaticStockData](Id)
     
 )
