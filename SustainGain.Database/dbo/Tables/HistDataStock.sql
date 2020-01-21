87a11c0bd3683fb4b69e345829f2eeb0e781ec5f
Daniel <daniel.lofqvist93@gmail.com>
2020-01-21 11:59:53 +0100

    Changed name for column StocksID -> StockID, for cosistancy

--- SustainGain.Database/dbo/Tables/HistDataStock.sql
+++ SustainGain.Database/dbo/Tables/HistDataStock.sql
@@ -1,9 +1,9 @@
 ﻿Create TABLE [dbo].[HistDataStocks]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [StocksID] INT NOT NULL, 
+    [StockID] INT NOT NULL, 
     [DateTime] DATETIME NOT NULL, 
     [CurrentPrice] MONEY NULL,
-    FOREIGN KEY (StocksID) REFERENCES [StaticStockData](Id)
+    FOREIGN KEY (StockID) REFERENCES [StaticStockData](Id)
     
 )
