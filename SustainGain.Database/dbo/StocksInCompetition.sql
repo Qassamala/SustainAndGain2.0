ffea5f80f9206a0d120085eacc7101e2b0834de3
MVikis <vikstenmartin@gmail.com>
2020-01-21 11:59:29 +0100

    added table: StocksInCompetition and added three foreign keys

---
+++ SustainGain.Database/dbo/StocksInCompetition.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[StocksInCompetition]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] NVARCHAR(450) NOT NULL, 
+    [CompId] INT NOT NULL, 
+    [StockId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    CONSTRAINT [FK_StocksInCompetition_ToTable] FOREIGN KEY ([CompId]) REFERENCES [Competition]([Id]), 
+    CONSTRAINT [FK_StocksInCompetition_ToTable_1] FOREIGN KEY ([StockId]) REFERENCES [StaticStockData]([Id]), 
+    CONSTRAINT [FK_StocksInCompetition_ToTable_2] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id]) 
+
+
+)
