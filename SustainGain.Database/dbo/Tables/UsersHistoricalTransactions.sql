1d2a690ee4a2813d06da1bf9aa2d2f83d6022199
MVikis <vikstenmartin@gmail.com>
2020-01-21 12:52:39 +0100

    Revert "Changed Datatypes"
    This reverts commit ff86feca82844b2df217fa0f24759193d4bd1358.

--- SustainGain.Database/dbo/Tables/UsersHistoricalTransactions.sql
+++ SustainGain.Database/dbo/Tables/UsersHistoricalTransactions.sql
@@ -3,9 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [UserId] NVARCHAR (450) not null foreign key references [AspNetUsers](Id),
     [CompetitionId] int not null foreign key references Competition(Id),
-    [Quantity] INT not NULL,
+    [Quantity] NCHAR(10) not NULL,
     [TransactionPrice] money not NULL,
     [StockId] int not null foreign key references StaticStockData(Id),
-    [DateTimeOfTransaction] DATETIME not null, 
-    [BuyOrSell] NCHAR(4) NOT NULL
+    [DateTimeOfTransaction] NCHAR(10) not null, 
+    [BuyOrSell] NCHAR(10) NOT NULL
 )
