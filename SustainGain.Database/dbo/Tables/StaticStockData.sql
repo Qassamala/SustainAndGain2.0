fe05111ce24a139971fed5b7b01abae6f1fe9c1a
Abdi <abdigobena@hotmail.com>
2020-01-21 11:28:00 +0100

    Added table StaticStockData, with columns, ID, Symbol, Description, Sector and CompanyName

---
+++ SustainGain.Database/dbo/Tables/StaticStockData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StaticStockData]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Symbol] VARCHAR(10) NOT NULL, 
+    [Description] VARCHAR(1500) NULL, 
+    [Sector] VARCHAR(64) NULL, 
+    [CompanyName] VARCHAR(64) NULL
+)
