c1e96c78ff3994f59027a7aa08fbebcb7d50e465
MVikis <vikstenmartin@gmail.com>
2020-01-22 13:04:48 +0100

    Added CompetitionVM and CompetitionsService and created 5 instances of Competition in database

---
+++ SustainAndGain/Models/CompetitionsService.cs
@@ -0,0 +1,44 @@
+﻿using SustainAndGain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SustainAndGain.Models
+{
+    public class CompetitionsService
+    {
+        private readonly SustainGainContext context;
+
+        public CompetitionsService(SustainGainContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddCompetition()
+        {
+            int month = 1;
+            
+            for (int i = 0; i < 5; i++)
+                
+            {
+                Competition competition = new Competition
+                {
+                    StartTime = new DateTime(2020, month, 23),
+                    EndTime = new DateTime(2020, month + 1, 23),
+              
+                };
+                month++;
+                context.Competition.Add(competition);
+                context.SaveChanges();
+            }
+            
+        }
+
+        public void DisplayCompetitions()
+        {
+
+        }
+
+    }
+}
---
+++ SustainAndGain/Models/ModelViews/CompetitionVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SustainAndGain.Models.ModelViews
+{
+    public class CompetitionVM
+    {
+        public CompetitionVM()
+        {
+            if (StartTime < DateTime.Now)
+            {
+                IsOngoing = true;
+            }
+        }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsOngoing { get; set; }
+    }
+}
