68c14f3a8d0e58b1cd5726f49738e95684786d91
Abdi <abdigobena@hotmail.com>
2020-01-23 21:27:44 +0100

    Added PortFolio VM and some initial props

---
+++ SustainAndGain/Models/ModelViews/PortfolioVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SustainAndGain.Models.ModelViews
+{
+    public class PortfolioVM
+    {
+        public decimal CurrentValue { get; set; }
+        public decimal AvailableCapital { get; set; }
+        public decimal InvestedCapital { get; set; }
+        //public List<Order> ListOfOrders { get; set; }
+    }
+}
