3bafe3a4835035d03271c5f02e6443142e0698e2
MVikis <vikstenmartin@gmail.com>
2020-01-26 22:59:08 +0100

    Have added alot of css code and images for userlayout and portfolio

--- SustainAndGain/Models/ModelViews/OrderVM.cs
+++ SustainAndGain/Models/ModelViews/OrderVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace SustainAndGain.Models.ModelViews
     public class OrderVM
     {
         public string Symbol { get; set; }
+        [Display(Name = "Order Value")]
         public decimal OrderValue { get; set; }
+        [Display(Name = "Time Of Insertion")]
         public DateTime TimeOfInsertion { get; set; }
+        [Display(Name = "Buy Or Sell")]
         public string BuyOrSell { get; set; }
     }
 }
--- SustainAndGain/Models/ModelViews/StockInfoVM.cs
+++ SustainAndGain/Models/ModelViews/StockInfoVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,19 @@ namespace SustainAndGain.Models.ModelViews
 {
     public class StockInfoVM
     {
+        [Display(Name ="Company Name")]
         public string CompanyName { get; set; }
+        [Display(Name = "Sustainable")]
         public bool? IsSustainable { get; set; }
+    
         public string Symbol { get; set; }
+        [Display(Name = "Last Price")]
         public decimal LastPrice { get; set; }
+        [Display(Name = "Last Updated")]
         public DateTime LastUpdated { get; set; }
+        [Display(Name = "Return This Year")]
         public decimal ReturnThisYear { get; set; }
+
         public string Description { get; set; }
         public string Sector { get; set; }
     }
