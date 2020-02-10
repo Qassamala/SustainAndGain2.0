fa1f8b125da3c598299db099ac9d83a543b08057
Abdi Gobena <abdigobena@hotmail.com>
2020-02-10 11:32:31 +0100

    Refactored code. ENtering competitions is not working

--- SustainAndGain/Controllers/StocksController.cs
+++ SustainAndGain/Controllers/StocksController.cs
@@ -10,50 +10,48 @@ using SustainAndGain.Models.ModelViews;
 
 namespace SustainAndGain.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class StocksController : Controller
     {
         private readonly StocksService service;
-        private readonly CompetitionsService competitionsService;
 
-        public StocksController(StocksService service, CompetitionsService competitionsService)
+        public StocksController(StocksService service)
         {
             this.service = service;
-            this.competitionsService = competitionsService;
         }
 
-       [AllowAnonymous]
-       [Route("Admin")]
-       public IActionResult Admin()
-       {
-           //competitionsService.DisplayCompetitions();
-           //competitionsService.AddCompetition();
-           //service.AddSustainProp();
-           //service.AddHistDataStocks();
-           //service.AddStaticStockData();
-           //service.GetCompanyDescription();
+        [AllowAnonymous]
+        [Route("Admin")]
+        public IActionResult Admin()
+        {
+            //competitionsService.DisplayCompetitions();
+            //competitionsService.AddCompetition();
+            //service.AddSustainProp();
+            //service.AddHistDataStocks();
+            //service.AddStaticStockData();
+            //service.GetCompanyDescription();
 
-           // Test reset 1
-           //service.AddStocksInComp();
+            // Test reset 1
+            //service.AddStocksInComp();
 
-           return View();
-       }
+            return View();
+        }
 
-       [Route("/chart/{id}")]
-       public IActionResult Chart(int id)
-       {
-           var viewmodel = service.GetHistoricalTransactionData(id);
+        [Route("/chart/{id}")]
+        public IActionResult Chart(int id)
+        {
+            var viewmodel = service.GetHistoricalTransactionData(id);
 
-           return PartialView("Chart", viewmodel);
-       }
+            return PartialView("Chart", viewmodel);
+        }
 
-       [Route("/financial/{id}")]
-       public IActionResult FinancialChart(int id)
-       {
-           var viewmodel = service.GetHistoricalTransactionData(id);
+        [Route("/financial/{id}")]
+        public IActionResult FinancialChart(int id)
+        {
+            var viewmodel = service.GetHistoricalTransactionData(id);
 
-           return PartialView("FinancialChart", viewmodel);
-       }
+            return PartialView("FinancialChart", viewmodel);
+        }
 
 
 
