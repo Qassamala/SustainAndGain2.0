3de44dbd585cf007e2cca8534ececf5f444e8134
Abdi Gobena <abdigobena@hotmail.com>
2020-02-10 21:40:21 +0100

    test

--- SustainAndGain/Startup.cs
+++ SustainAndGain/Startup.cs
@@ -29,7 +29,7 @@ namespace SustainAndGain
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
         {
             //// Adding Quartz services
             //services.AddSingleton<IJobFactory, SingletonJobFactory>();
